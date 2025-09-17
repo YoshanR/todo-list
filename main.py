43a2bfae6c640453b70ce190089c83a569e3779a
Yoshan <yoshanravishanker@gmail.com>
2025-09-17 21:54:00 +0100

    Add Task Function Created

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-<<<<<<< HEAD
 
+# Main window creation
 import tkinter as tk
+
+
 
 root = tk.Tk()
 root.title("To-Do List App")
 root.geometry("300x400")
 
+
+
+# Add Task Function
+def add_task():
+    task = task_entry.get()
+    if task:
+        task_listbox.insert(tk.END, task)
+        task_entry.delete(0, tk.END)
+
+
+
+# Task entry
+
 root.mainloop()
-=======
-print("Hello, world!)
->>>>>>> e29a7118ffc115eafeaeae893f4cb362be5e1881
+
